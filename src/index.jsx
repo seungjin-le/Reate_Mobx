bb871970e0e2da17cb246e63d337f271b353944c
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-05 22:16:02 +0900

    decorators 설정

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { autorun, makeObservable, observable } from 'mobx';
 
+const isLogin = observable(true);
+const person = observable({
+  name: 'mark',
+  age: 30,
+});
+class PersonStore {
+  @observable
+  name = 'Two';
+
+  @observable
+  age = 20;
+
+  constructor() {
+    makeObservable(this);
+  }
+}
+const personStore = new PersonStore();
+
+autorun(() => {
+  console.log(isLogin.get());
+  console.log(person.age);
+  console.log(personStore.age);
+});
+
+isLogin.set(false);
+person.age = 40;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
